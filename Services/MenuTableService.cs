67cc1c4be530d9fb2156a96b15e6198bc3fb2e1d
ST10486778 <ST10486778@VCCONNECT.edu.za>
2026-09-12 16:49:41 +0200

    Implement MenuTableService for Azure Table CRUD operations
    Implemented the MenuTableService repository pattern using TableClient to auto-create the MenuItems table and perform asynchronous CRUD operations (AddOrUpdateMenuItemAsync, GetAllMenuItemsAsync, GetMenuItemAsync, DeleteMenuItemAsync).

---
+++ Services/MenuTableService.cs
@@ -0,0 +1,56 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using CoffeeNChill.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoffeeNChill.Services
+{
+    public class MenuTableService
+    {
+        private readonly TableClient _tableClient;
+
+        public MenuTableService(string connectionString, string tableName = "MenuItems")
+        {
+            var serviceClient = new TableServiceClient(connectionString);
+            _tableClient = serviceClient.GetTableClient(tableName);
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddOrUpdateMenuItemAsync(MenuItem menuItem)
+        {
+            await _tableClient.UpsertEntityAsync(menuItem, TableUpdateMode.Replace);
+        }
+
+        public async Task<List<MenuItem>> GetAllMenuItemsAsync()
+        {
+            var menuItems = new List<MenuItem>();
+            AsyncPageable<MenuItem> queryResults = _tableClient.QueryAsync<MenuItem>();
+
+            await foreach (var item in queryResults)
+            {
+                menuItems.Add(item);
+            }
+
+            return menuItems;
+        }
+
+        public async Task<MenuItem?> GetMenuItemAsync(string category, string rowKey)
+        {
+            try
+            {
+                Response<MenuItem> response = await _tableClient.GetEntityAsync<MenuItem>(category, rowKey);
+                return response.Value;
+            }
+            catch (RequestFailedException ex) when (ex.Status == 404)
+            {
+                return null;
+            }
+        }
+
+        public async Task DeleteMenuItemAsync(string category, string rowKey)
+        {
+            await _tableClient.DeleteEntityAsync(category, rowKey);
+        }
+    }
+}
